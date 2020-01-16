1d68729e5c5d44f44989d691c1bd4103b90cf4cd
Zero87856 <87856@farnborough.ac.uk>
2020-01-16 13:50:01 +0000

    javascript/html files made

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -2,13 +2,15 @@ function pageLoad() {
 
     let now = new Date();
 
-    let myHTML = '<div style="text-align:center;">'
-        + '<h1>Welcome to my API powered website!</h1>'
-        + '<img src="/client/img/logo.png"  alt="Logo"/>'
-        + '<div style="font-style: italic;">'
-        + 'Generated at ' + now.toLocaleTimeString()
+    let myHTML = '<div style="text-align:left;">'
+        + '<h1>Zero</h1>'
         + '</div>'
         + '</div>';
+
+    let myHTML2 = '<div style="text-align:right;">'
+        +'<body>'
+        +'Generated at ' + now.toLocaleTimeString();
+
 
     document.getElementById("testDiv").innerHTML = myHTML;
 
